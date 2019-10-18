315f90ff84626c75f26b6ef3e175a11576ff8db8
Jimit Patel <40808237+jimit7@users.noreply.github.com>
2019-10-18 11:55:23 -0400

    Create hello_world_module.c

---
+++ hello_world_module.c
@@ -0,0 +1,26 @@
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+
+
+static int __init hello_world_init(void)
+{
+	printk(KERN_INFO "Welcome to EmbeTronicX\n");
+        printk(KERN_INFO "This is the Simple Module\n");
+        printk(KERN_INFO "Kernel Module Inserted Successfully...\n");
+	return 0;
+}
+
+void __exit hello_world_exit(void)
+{
+	printk(KERN_INFO "Kernel Module Removed Successfully...\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JIMIT_PATEL <jimitypatel1995@gmail.com> ");
+MODULE_DESCRIPTION("A simple hello world driver");
+MODULE_VERSION("1.0.0");
